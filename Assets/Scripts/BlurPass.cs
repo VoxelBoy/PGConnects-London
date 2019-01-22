3b478c645612dbf25fbe3848c350db572f55e646
Yilmaz Kiymaz <yilmaz.kiymaz@innogames.com>
2019-01-22 12:26:41 +0000

    Added blur pass example

---
+++ Assets/Scripts/BlurPass.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.LWRP;
+
+public class BlurPass : MonoBehaviour, IAfterTransparentPass
+{
+    private BlurPassImpl blurPass;
+
+    [SerializeField]
+    private Color tintColor;
+    
+    [SerializeField]
+    private float blurSize;
+    
+    [SerializeField]
+    private float blurSigma;
+
+    /// <summary>
+    /// DO NOT REMOVE.
+    /// Without the Start method, the Inspector won't show the enabled/disabled checkbox for this component.
+    /// </summary>
+    private void Start()
+    {
+        
+    }
+
+    public ScriptableRenderPass GetPassToEnqueue(RenderTextureDescriptor baseDescriptor, RenderTargetHandle colorHandle, RenderTargetHandle depthHandle)
+    {
+        if (blurPass == null)
+        {
+            blurPass = new BlurPassImpl(colorHandle, tintColor, blurSize, blurSigma);
+        }
+
+        blurPass.SetState(enabled, tintColor, blurSize, blurSigma);
+            
+        return blurPass;
+    }
+}
+
+public class BlurPassImpl : ScriptableRenderPass
+{
+    const string k_RenderGrabPassTag = "BlurPass";
+
+    private RenderTargetHandle m_ColorHandle;
+
+    private readonly Material m_Material;
+    private int screenCopyID;
+    
+    private bool enabled;
+    private Color tintColor;
+    private float blurSize;
+    private float blurSigma;
+
+    public BlurPassImpl(RenderTargetHandle colorHandle, Color tintColor, float blurSize, float blurSigma)
+    {
+        this.blurSigma = blurSigma;
+        this.blurSize = blurSize;
+        this.tintColor = tintColor;
+        m_ColorHandle = colorHandle;
+
+        m_Material = CoreUtils.CreateEngineMaterial(Shader.Find("_PGC/LWRP/Blur"));
+        m_Material.SetColor("_Color", tintColor);
+        m_Material.SetFloat("_Size", blurSize);
+        m_Material.SetFloat("_Sigma", blurSigma);
+    }
+
+    public override void Execute(ScriptableRenderer renderer, ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        if (enabled == false)
+        {
+            return;
+        }
+        
+        #if UNITY_EDITOR
+        m_Material.SetColor("_Color", tintColor);
+        m_Material.SetFloat("_Size", blurSize);
+        m_Material.SetFloat("_Sigma", blurSigma);
+        #endif
+        
+        CommandBuffer cmd = CommandBufferPool.Get(k_RenderGrabPassTag);
+
+        using (new ProfilingSample(cmd, k_RenderGrabPassTag))
+        {
+            // copy screen into temporary RT
+            screenCopyID = Shader.PropertyToID("_ScreenCopyTexture");
+            RenderTextureDescriptor opaqueDesc = ScriptableRenderer.CreateRenderTextureDescriptor(ref renderingData.cameraData);
+            
+            cmd.GetTemporaryRT(screenCopyID, opaqueDesc, FilterMode.Bilinear);
+            cmd.Blit(m_ColorHandle.Identifier(), screenCopyID);
+
+            cmd.SetGlobalVector("offsets", new Vector4(2.0f / Screen.width, 0, 0, 0));
+            cmd.Blit(screenCopyID, m_ColorHandle.Identifier(), m_Material);
+        }
+
+        context.ExecuteCommandBuffer(cmd);
+        CommandBufferPool.Release(cmd);
+    }
+
+    public override void FrameCleanup(CommandBuffer cmd)
+    {
+        cmd?.ReleaseTemporaryRT(screenCopyID);
+    }
+
+    public void SetState(bool enabled, Color tintColor, float blurSize, float blurSigma)
+    {
+        this.enabled = enabled;
+        this.blurSigma = blurSigma;
+        this.blurSize = blurSize;
+        this.tintColor = tintColor;
+    }
+}
---
+++ Assets/Shaders/Blur.shader
@@ -0,0 +1,93 @@
+﻿Shader "_PGC/LWRP/Blur" {
+    Properties {
+        [PerRendererData] _MainTex ("Main Texture", 2D) = "white" {}
+        _Color ("Color", Color) = (1,1,1,1)
+        _Size ("Size", Range(0, 20)) = 1
+        _Sigma ("Sigma", Range(0, 3)) = 0.8
+    }
+ 
+    Category {
+ 
+        Tags { "Queue"="Transparent" "RenderType" = "Transparent" "RenderPipeline" = "LightweightPipeline" "IgnoreProjector" = "True"}
+ 
+        SubShader {
+        
+            Blend SrcAlpha OneMinusSrcAlpha
+            
+            Pass {
+                Name "ForwardLit"
+                Tags { "LightMode" = "LightweightForward" }
+             
+                HLSLPROGRAM
+                
+                // Required to compile gles 2.0 with standard srp library
+                #pragma prefer_hlslcc gles
+                #pragma exclude_renderers d3d11_9x
+                #pragma target 2.0
+                
+                #pragma vertex vert
+                #pragma fragment frag
+                #pragma fragmentoption ARB_precision_hint_fastest
+                #include "UnityCG.cginc"
+                
+                struct appdata_t {
+                    float4 vertex : POSITION;
+                    float4 color    : COLOR;
+                    float2 texcoord: TEXCOORD0;
+                };
+             
+                struct v2f {
+                    float4 vertex : POSITION;
+                    half4 color    : COLOR0;
+                    float4 uvgrab : TEXCOORD0;
+                };
+                
+                v2f vert (appdata_t v) {
+                    v2f o;
+                    o.vertex = UnityObjectToClipPos(v.vertex);
+                    o.uvgrab = ComputeGrabScreenPos(o.vertex);
+                    o.color = v.color;
+                    return o;
+                }
+             
+                half4 _Color;
+                sampler2D _MainTex;
+                float4 _MainTex_TexelSize;
+                float _Size;
+                float _Sigma;
+                
+                #define PI 3.14159265
+                #define KERNEL_SIZE 4
+                
+                float gauss(float x, float y, float sigma) {
+                    return 1.0 / (2.0 * PI * sigma * sigma) * exp(-(x * x + y * y) / (2.0 * sigma * sigma));
+                }
+             
+                half4 frag( v2f i ) : COLOR {
+                
+                    half4 o = 0;
+                    float sum = 0;
+                    float4 uvOffset;
+                    float weight;
+                    
+                    for(int x = -KERNEL_SIZE / 2; x <= KERNEL_SIZE / 2; ++x)
+                        for(int y = -KERNEL_SIZE / 2; y <= KERNEL_SIZE / 2; ++y)
+                        {
+                            uvOffset = i.uvgrab;
+                            uvOffset.x += x * _MainTex_TexelSize.x * _Size;
+                            uvOffset.y += y * _MainTex_TexelSize.y * _Size;
+                            weight = gauss(x, y, _Sigma);
+                            o += tex2D(_MainTex, uvOffset) * weight;
+                            sum += weight;
+                        }
+                    o *= (1.0 / sum);
+                    o.rgb *= _Color;
+                    o.a = i.color.a;
+                    return o;
+                }
+                
+                ENDHLSL
+            }
+        }
+    }
+}
